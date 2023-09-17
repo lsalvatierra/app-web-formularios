edcb8f50bb2abfb0fa24d1438093db7ceaa89a8f
lsalvatierra89 <l.salvat@gmail.com>
2023-09-17 11:01:34 -0500

    formulario con bootstrap

---
+++ src/main/java/pe/edu/cibertec/appwebformularios/controller/FormSumaController.java
@@ -0,0 +1,14 @@
+package pe.edu.cibertec.appwebformularios.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FormSumaController {
+
+    @GetMapping("/promedionotas")
+    public String index(){
+        return "formSuma";
+    }
+
+}
