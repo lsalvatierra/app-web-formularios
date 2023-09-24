126c028fe1ac14f2845e37357aa33d22c751b14b
lsalvatierra89 <l.salvat@gmail.com>
2023-09-24 12:19:25 -0500

    ejercicio 3 terminado

---
+++ src/main/java/pe/edu/cibertec/appwebformularios/model/PrimoModel.java
@@ -0,0 +1,14 @@
+package pe.edu.cibertec.appwebformularios.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PrimoModel {
+    private Integer numero;
+}
